e48b6768b44108ca3efb3f83c269a7123a4e2912
s-eguchi3304 <s-eguchi@point-inc.jp>
2023-02-14 15:02:24 +0900

    テンプレフォーム修正

--- src/pages/InputForm.vue
+++ src/pages/InputForm.vue
@@ -1,8 +1,29 @@
 <script setup>
-import { ref } from 'vue';
+import { ref, reactive } from 'vue';
 
 const title = ref('テストフォーム');
-const textInput = ref('');
+
+const formDate = reactive({
+  text: '',
+  textArea: '',
+  check: [],
+  radio: '',
+  select: '',
+  selects: [],
+  tel: '',
+});
+
+const submitForm = () => {
+  console.log('submit', formDate);
+};
+
+/*const textInput = ref('');
+const textAreaInput = ref('');
+const checkValue = ref([]);
+const radioValue = ref('');
+const selectValue = ref('');
+const selectValues = ref([]);
+const telValue = ref('');*/
 </script>
 
 <template>
@@ -12,52 +33,144 @@ const textInput = ref('');
         <div class="row border">
           <div class="col-md">
             <h2 class="mt-3 mb-3">{{ title }}</h2>
-            <form>
+            <form @submit.prevent="submitForm">
               <div class="form-group mb-3">
                 <label>フォームテキスト</label>
                 <input
                   class="form-control"
                   type="text"
                   id="text"
-                  :value="textInput"
-                  @input="textInput = $event.target.value"
+                  v-model="formDate.text"
                 />
               </div>
 
               <div class="form-group mb-3">
                 <label>テキストエリア</label>
-                <textarea class="form-control" rows="3"></textarea>
+                <textarea
+                  class="form-control"
+                  rows="3"
+                  v-model="formDate.textarea"
+                  id="textarea"
+                ></textarea>
+              </div>
+
+              <div class="mb-3">
+                <p class="mb-0">チェックボックス</p>
+                <div class="form-check form-check-inline">
+                  <input
+                    class="form-check-input"
+                    type="checkbox"
+                    id="Checkbox1"
+                    v-model="formDate.check"
+                    value="ラグビー"
+                  />
+                  ラグビー
+                </div>
+                <div class="form-check form-check-inline">
+                  <input
+                    class="form-check-input"
+                    type="checkbox"
+                    id="Checkbox2"
+                    v-model="formDate.check"
+                    value="サッカー"
+                  />
+                  サッカー
+                </div>
+                <div class="form-check form-check-inline">
+                  <input
+                    class="form-check-input"
+                    type="checkbox"
+                    id="Checkbox3"
+                    v-model="formDate.check"
+                    value="バスケットボール"
+                  />
+                  バスケットボール
+                </div>
+              </div>
+
+              <div class="mb-3">
+                <p class="mb-0">ラジオボタン</p>
+                <div class="form-check form-check-inline">
+                  <input
+                    class="form-check-input"
+                    type="radio"
+                    id="radiobtn1"
+                    v-model="formDate.radio"
+                    value="ラグビー"
+                  />
+                  ラグビー
+                </div>
+                <div class="form-check form-check-inline">
+                  <input
+                    class="form-check-input"
+                    type="radio"
+                    id="radiobtn2"
+                    v-model="formDate.radio"
+                    value="サッカー"
+                  />
+                  サッカー
+                </div>
+                <div class="form-check form-check-inline">
+                  <input
+                    class="form-check-input"
+                    type="radio"
+                    id="radiobtn3"
+                    v-model="formDate.radio"
+                    value="バスケットボール"
+                  />
+                  バスケットボール
+                </div>
               </div>
 
               <div class="form-group mb-3">
-                <label>部署：</label>
-                <select class="form-control">
-                  <option></option>
-                  <option></option>
-                  <option></option>
-                  <option></option>
+                <label>セレクトボックス</label>
+                <select class="form-select" v-model="formDate.select">
+                  <option disabled value="">選択</option>
+                  <option>値1</option>
+                  <option>値2</option>
+                  <option>値3</option>
                 </select>
               </div>
+
               <div class="form-group mb-3">
-                <label>住所：</label>
-                <input type="text" class="form-control" placeholder="" />
+                <label>複数選択ボックス</label>
+                <select
+                  class="form-select"
+                  v-model="formDate.selects"
+                  multiple
+                  size="3"
+                >
+                  <option>値1</option>
+                  <option>値2</option>
+                  <option>値3</option>
+                  <option>値4</option>
+                  <option>値5</option>
+                </select>
               </div>
+
               <div class="form-group mb-3">
                 <label>電話番号：</label>
                 <input
-                  type="text"
+                  type="tel"
+                  size="15"
+                  maxlength="15"
                   class="form-control"
                   placeholder="XXX-XXXX-XXXX"
+                  v-model="formDate.tel"
                 />
               </div>
-            </form>
-            <div class="row justify-content-center text-center mb-3">
-              <div class="col-md-4">
-                <button type="submit" class="btn btn-outline-primary btn-block">
-                  送信
-                </button>
+
+              <div class="row justify-content-center text-center mb-3">
+                <div class="col-md-4">
+                  <button
+                    type="submit"
+                    class="btn btn-outline-primary btn-block"
+                  >
+                    送信
+                  </button>
+                </div>
               </div>
-            </div>
+            </form>
           </div>
         </div>
       </div>
