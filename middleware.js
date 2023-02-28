dc9c87314657902b2396cc5a9be654365d8493de
s-eguchi3304 <s-eguchi@point-inc.jp>
2023-02-28 12:05:38 +0900

    basic認証

--- middleware.js
+++ middleware.js
@@ -4,7 +4,7 @@ export const config = {
   matcher: '/',
 };
 
-export function middleware(request) {
+export default function middleware(request) {
   const authorizationHeader = request.headers.get('authorization');
 
   if (authorizationHeader) {
